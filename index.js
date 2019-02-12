1ebd198fbdc765c5b26736c10250fd4ed439a8d3
Felice Forby <feliceforby@gmail.com>
2019-02-12 06:43:31 +0000

    set up classes

--- index.js
+++ index.js
@@ -1,2 +1,55 @@
 // global datastore
-let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let store = {
+  neighborhoods: [],
+  meals: [],
+  customers: [],
+  deliveries: []
+};
+
+// Neighborhood class
+// -------------------------------
+let neighborhoodId = 0;
+
+class Neighborhood {
+  constructor(name) {
+    this.id = ++neighborhoodId;
+    this.name = name;
+  }
+}
+
+// Customer class
+// -------------------------------
+let customerId = 0;
+
+class Customer {
+  constructor(name, neighborhood) {
+    this.id = ++customerId;
+    this.name = name;
+    this.neighborhoodId = neighborhood.id;
+  }
+}
+
+// Meal class
+// -------------------------------
+let mealId = 0;
+
+class Meal {
+  constructor(title, price) {
+    this.id = ++mealId;
+    this.title = title;
+    this.price = price;
+  }
+}
+
+// Delivery class
+// -------------------------------
+let deliveryId = 0;
+
+class Delivery {
+  constructor(meal, neighborhood, customer) {
+    this.id = ++deliveryId;
+    this.mealId = meal.id;
+    this.neighborhoodId = neighborhood.id;
+    this.customerId = customer.id;
+  }
+}

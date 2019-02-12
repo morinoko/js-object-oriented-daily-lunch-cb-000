b2d01d11a08ca3a5b08c56748a96f12a295557e2
Felice Forby <feliceforby@gmail.com>
2019-02-12 07:52:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ class Meal {
 let deliveryId = 0;
 
 class Delivery {
-  constructor(meal, neighborhood, customer) {
+  constructor(mealId, neighborhoodId, customerId) {
     this.id = ++deliveryId;
-    this.mealId = meal.id;
-    this.neighborhoodId = neighborhood.id;
-    this.customerId = customer.id;
+    this.mealId = mealId;
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
 
     store.deliveries.push(this);
   }
